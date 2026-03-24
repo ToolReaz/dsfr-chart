eeda2191b32692a8fe975223daa22a895fd9a427
ToolReaz <toolreaz@pm.me>
2026-03-24 02:39:49 +0100

    Try upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
-// /** @type { import('@storybook/web-components-vite').StorybookConfig } */
+/** @type { import('@storybook/svelte-vite').StorybookConfig } */
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs'],
+  framework: '@storybook/svelte-vite',
   core: {
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
